e388214d402679705eac1fee00245f1e880b33a9
Wilhelm Eklund <wilhelm.eklund@barnebys.com>
2022-01-17 18:14:20 +0100

    Footer with link to our Github

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['github.githubassets.com'],
+  },
 }
 
 module.exports = nextConfig
