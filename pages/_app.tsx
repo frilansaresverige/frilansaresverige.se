09e5c11d40c8928d2e61222684f8c5caab9dd0d6
Wilhelm Eklund <wilhelm.eklund@barnebys.com>
2022-01-29 14:52:45 +0100

    Use _document.tsx for things shown on each page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,33 @@
 import '../styles/globals.css'
+
 import type { AppProps } from 'next/app'
+import Image from 'next/image'
+
+import styles from '../styles/Home.module.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <footer className={styles.footer}>
+        <a
+          href="https://github.com/frilansaresverige/frilansaresverige.se/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Bidra till sidan genom vår GitHub 👉
+          <span className={styles.logo}>
+            <Image
+              alt="Github Logo"
+              src="https://github.githubassets.com/images/modules/site/icons/footer/github-mark.svg"
+              width={20}
+              height={20}
+            />
+          </span>
+        </a>
+      </footer>
+    </>
+  )
 }
 
 export default MyApp
---
+++ pages/_document.tsx
@@ -0,0 +1,22 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <title>Frilansare Sverige</title>
+          <meta
+            name="description"
+            content="Frilansare Sverige är Sveriges största community för frilansare."
+          />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
