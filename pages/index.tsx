8dc2ea409efd37e6e342933d1ee1ed35d0b79cad
Wilhelm Eklund <wilhelm.eklund@barnebys.com>
2022-03-05 15:04:37 +0100

    Fix the link to https://uppdrag.frilansaresverige.se/

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,50 +15,66 @@ const Home: NextPage = () => {
 
       <p className="description">
         Vi är Sveriges största community för frilansare med över{' '}
-        {nbrFreelansers} medlemmar! Vårt syfte är att främja kontaktskapande
-        och uppdragstipsande mellan frilansare.
+        {nbrFreelansers} medlemmar! Vårt syfte är att främja kontaktskapande och
+        uppdragstipsande mellan frilansare.
       </p>
 
       <div className={styles.grid}>
         <div className={styles.card}>
           <h2>Vi hjälper varandra</h2>
           <p>
-            Vi hjälper varandra med allt som rör livet som frilansare! T ex
-            hur man hittar uppdrag och hur man bokför saker.
+            Vi hjälper varandra med allt som rör livet som frilansare! T ex hur
+            man hittar uppdrag och hur man bokför saker.
           </p>
 
           <h2>Ett Slack-community</h2>
           <p>Frilansare från hela Sverige är välkomna.</p>
 
-
           <Link href="/ansokan">
             <a className="primary-button">
               Ansök om medlemsskap
-              <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M3 13L17.17 13L11.59 18.59L13 20L21 12L13 4L11.59 5.41L17.17 11L3 11V13Z" fill="#333333"/>
+              <svg
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M3 13L17.17 13L11.59 18.59L13 20L21 12L13 4L11.59 5.41L17.17 11L3 11V13Z"
+                  fill="#333333"
+                />
               </svg>
             </a>
           </Link>
         </div>
 
+        <div className={styles.card}>
+          <h2>Vill du ha hjälp med något? &rarr;</h2>
+          <p>
+            Om du jobbar på ett bolag som har konsultbehov så kan du gratis nå
+            ut till över {nbrFreelansers} frilansare med informationen, utan
+            mellanhänder.
+          </p>
 
-          <div className={styles.card}>
-            <h2>Vill du ha hjälp med något? &rarr;</h2>
-            <p>
-              Om du jobbar på ett bolag som har konsultbehov så kan du gratis
-              nå ut till över {nbrFreelansers} frilansare med informationen,
-              utan mellanhänder.
-            </p>
-
-            <Link href="/ansokan">
+          <Link href="https://uppdrag.frilansaresverige.se/">
             <a className="primary-button">
               Tipsa om konsultuppdrag
-              <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M3 13L17.17 13L11.59 18.59L13 20L21 12L13 4L11.59 5.41L17.17 11L3 11V13Z" fill="#333333"/>
+              <svg
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M3 13L17.17 13L11.59 18.59L13 20L21 12L13 4L11.59 5.41L17.17 11L3 11V13Z"
+                  fill="#333333"
+                />
               </svg>
             </a>
           </Link>
-          </div>
+        </div>
       </div>
     </div>
   )
